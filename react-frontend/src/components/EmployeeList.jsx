77a94805ec58c232196244aa0e873952dd4f6b17
VincentHsu <jway.vincenthsu@gmail.com>
2021-12-15 13:27:33 +0800

    react-frontend add employeeList component

---
+++ react-frontend/src/components/EmployeeList.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class EmployeeList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees:[],
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1 className="text-center">Employee List</h1>
+                <div className="row">
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Employee First Name</th>
+                                <th>Employee Last Name</th>
+                                <th>Employee Email</th>
+                                <th>Employee Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.employees.map(
+                                     employee => 
+                                     <tr key={employee.id}>
+                                         <td>{employee.firstName}</td>
+                                         <td>{employee.lastName}</td>
+                                         <td>{employee.email}</td>
+                                     </tr>
+                                    )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default EmployeeList;
